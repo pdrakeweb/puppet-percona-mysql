9af4e5fe85e35c17080ff04f29952328b4dd0426
Peter Drake <pdrake@allplayers.com>
2013-01-16 10:25:33 -0500

    [#42568905] Add parameter for privileges.

--- manifests/lib.pp
+++ manifests/lib.pp
@@ -2,10 +2,10 @@ class mysqllib {
 
   include mysql::client
 
-  define mysqldb( $user, $password, $db = $name, $clienthost = 'localhost', $host = 'localhost' ) {
+  define mysqldb( $user, $password, $db = $name, $clienthost = 'localhost', $host = 'localhost', $privileges = 'ALL' ) {
     exec { "create-${db}-db-${clienthost}":
       unless => "/usr/bin/mysql -h${host} -uroot -e \"SHOW GRANTS FOR '${user}'@'${clienthost}';\" | grep 'ON `${db}`.*'",
-      command => "/usr/bin/mysql -h${host} -uroot -e \"CREATE DATABASE IF NOT EXISTS ${db}; GRANT ALL ON ${db}.* TO '${user}'@'${clienthost}' IDENTIFIED BY '${password}'; FLUSH PRIVILEGES;\"",
+      command => "/usr/bin/mysql -h${host} -uroot -e \"CREATE DATABASE IF NOT EXISTS ${db}; GRANT ${privileges} ON ${db}.* TO '${user}'@'${clienthost}' IDENTIFIED BY '${password}'; FLUSH PRIVILEGES;\"",
       require => Service["mysql"],
     }
   }
