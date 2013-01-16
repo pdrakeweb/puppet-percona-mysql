c82562f58aa269d5fa22c22bc700b71cfc730b47
Peter Drake <pdrake@allplayers.com>
2013-01-16 10:21:06 -0500

    [#42568905] Enable multiple grants for a single database.

--- manifests/lib.pp
+++ manifests/lib.pp
@@ -2,10 +2,10 @@ class mysqllib {
 
   include mysql::client
 
-  define mysqldb( $user, $password, $clienthost = 'localhost', $host = 'localhost' ) {
-    exec { "create-${name}-db-${clienthost}":
-      unless => "/usr/bin/mysql -h${host} -uroot -e \"SHOW GRANTS FOR '${user}'@'${clienthost}';\" | grep 'ON `${name}`.*'",
-      command => "/usr/bin/mysql -h${host} -uroot -e \"CREATE DATABASE IF NOT EXISTS ${name}; GRANT ALL ON ${name}.* TO '${user}'@'${clienthost}' IDENTIFIED BY '${password}'; FLUSH PRIVILEGES;\"",
+  define mysqldb( $user, $password, $db = $name, $clienthost = 'localhost', $host = 'localhost' ) {
+    exec { "create-${db}-db-${clienthost}":
+      unless => "/usr/bin/mysql -h${host} -uroot -e \"SHOW GRANTS FOR '${user}'@'${clienthost}';\" | grep 'ON `${db}`.*'",
+      command => "/usr/bin/mysql -h${host} -uroot -e \"CREATE DATABASE IF NOT EXISTS ${db}; GRANT ALL ON ${db}.* TO '${user}'@'${clienthost}' IDENTIFIED BY '${password}'; FLUSH PRIVILEGES;\"",
       require => Service["mysql"],
     }
   }
