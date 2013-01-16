40a83371a8dad2c9e3c366731eea2da51ee4e29a
Peter Drake <pdrake@allplayers.com>
2013-01-16 09:37:05 -0500

    [#42568905] Add client host specific grants checking.

--- manifests/lib.pp
+++ manifests/lib.pp
@@ -4,8 +4,8 @@ class mysqllib {
 
   define mysqldb( $user, $password, $clienthost = 'localhost', $host = 'localhost' ) {
     exec { "create-${name}-db-${clienthost}":
-      unless => "/usr/bin/mysql -h${host} -u${user} -p${password} ${name}",
-      command => "/usr/bin/mysql -h${host} -uroot -e \"create database if not exists ${name}; grant all on ${name}.* to '${user}'@'${clienthost}' identified by '${password}'; flush privileges;\"",
+      unless => "/usr/bin/mysql -h${host} -uroot -e \"SHOW GRANTS FOR '${user}'@'${clienthost}';\" | grep 'ON `${name}`.*'",
+      command => "/usr/bin/mysql -h${host} -uroot -e \"CREATE DATABASE IF NOT EXISTS ${name}; GRANT ALL ON ${name}.* TO '${user}'@'${clienthost}' IDENTIFIED BY '${password}'; FLUSH PRIVILEGES;\"",
       require => Service["mysql"],
     }
   }
